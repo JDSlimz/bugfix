ef543853acb8f17c739d9c203d6d45b38a8b6d9f
Josh <joshuagreen118@gmail.com>
2017-01-28 21:18:24 -0500

    globalized bugs

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
+var bugs = [];
+
 $(document).ready(function(){
-	var bugs = [];
+	
 	var i = 0;
 	while(i < 8){
 		bugs.push(Math.floor(Math.random()*900) + 100);
 		i++;
 	}
 	
-	add_elements(bugs);
+	add_elements();
 });
 
-function add_elements(bugs){
+function add_elements(){
 	for (var i = 0; i < bugs.length; i++) {
 		$('body').append('Bug# ' + bugs[i] + ': <input id="toggle-' + bugs[i] + '" class="bug" type="checkbox" data-toggle="toggle"><br>');
 	}
 	
 	$('.bug').bootstrapToggle();
 	
-	select_starting_bug(bugs);
+	select_starting_bug();
 }
 
-function select_starting_bug(bugs){
+function select_starting_bug(){
 	var bug_index = Math.floor(Math.random()*bugs.length);
 	jQuery('#toggle-'+bugs[bug_index]).bootstrapToggle('on');
 }
+

3f6cd80b0573297b07ffec679d71104c7c55353a
Josh <joshuagreen118@gmail.com>
2017-01-28 20:39:11 -0500

    generate bug toggles

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-	//var bugs = [];
+	var bugs = [];
 	var i = 0;
 	while(i < 8){
-		//bugs.push(Math.floor(Math.random()*900) + 100);
-		console.log(Math.floor(Math.random()*900) + 100);
+		bugs.push(Math.floor(Math.random()*900) + 100);
 		i++;
 	}
-	 console.log(bugs);
+	
+	add_elements(bugs);
 });
+
+function add_elements(bugs){
+	var i = 0;
+	while(i < bugs.length()){
+		$(body).append('Bug# ' + bugs[i] + ': <input id="toggle-demo" type="checkbox" data-toggle="toggle">');
+	}
+}

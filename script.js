4edfa9274f6725937a639a9e611ecf347256cb87
Josh <joshuagreen118@gmail.com>
2017-01-28 20:22:26 -0500

    fixed bug....

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
 	var bugs = [];
 	var i = 0;
 	while(i < 8){
-		//bugs.push(Math.floor(Math.random()*900) + 100);
-		
-		console.log(Math.floor(Math.random()*900) + 100);
+		bugs.push(Math.floor(Math.random()*900) + 100);
+		i++;
 	}
 	 console.log(bugs);
 });

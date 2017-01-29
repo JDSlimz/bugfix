ae163b33e3f908e353068afd66ea894f67d9d596
Josh <joshuagreen118@gmail.com>
2017-01-28 20:20:35 -0500

    testing

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 	var bugs = [];
 	var i = 0;
 	while(i < 8){
-		bugs.push(Math.floor(Math.random()*900) + 100);
+		//bugs.push(Math.floor(Math.random()*900) + 100);
+		
+		console.log(Math.floor(Math.random()*900) + 100);
 	}
 	 console.log(bugs);
 });

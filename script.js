96fc5ac5d036d20213e3217035ff1dd8e058a9d1
Josh <joshuagreen118@gmail.com>
2017-01-28 20:19:23 -0500

    oops

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 	var bugs = [];
-	var i = 0
+	var i = 0;
 	while(i < 8){
 		bugs.push(Math.floor(Math.random()*900) + 100);
 	}
-	Math.floor(Math.random()*900) + 100;
 	 console.log(bugs);
 });

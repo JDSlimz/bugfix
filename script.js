1f46fb2956bdea4a205afe4e8b8cd86a598f2256
Josh Green <joshuagreen118@gmail.com>
2017-01-28 20:27:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	//var bugs = [];
+	var bugs = [];
 	var i = 0;
 	while(i < 8){
-		//bugs.push(Math.floor(Math.random()*900) + 100);
+		bugs.push(Math.floor(Math.random()*900) + 100);
 		console.log(Math.floor(Math.random()*900) + 100);
 		i++;
 	}

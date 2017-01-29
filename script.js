b9639a2d98dfe3fe7622d9e1c0e31ae5e685bdd2
Josh <joshuagreen118@gmail.com>
2017-01-28 20:17:14 -0500

    make bugs

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+	var bugs = [];
+	var i = 0
+	while(i < 8){
+		bugs.push(Math.floor(Math.random()*900) + 100);
+	}
+	Math.floor(Math.random()*900) + 100;
+	 console.log(bugs);
 });
